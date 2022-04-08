826b1ba3b327d1936da62f50c6ab12017084e237
JL-EFR <98639258+JL-EFR@users.noreply.github.com>
2022-04-08 22:04:11 +0700

    updated to lasted main

--- python/Cat_VS_Dog.py
+++ python/Cat_VS_Dog.py
@@ -9,6 +9,7 @@ pygame.font.init()
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Cat VS Dog")
+
 mcu = McuBoard(find_mcu_boards()[0])
 peri = PeriBoard(mcu)
 
@@ -24,6 +25,7 @@ WALL_WIDTH = 30
 WALL_HEIGHT = HEIGHT//3
 WALL = pygame.Rect(WIDTH//2 - WALL_WIDTH//2, HEIGHT -
                    WALL_HEIGHT, WALL_WIDTH, WALL_HEIGHT)
+WHITERECT = pygame.Rect(0, 0, WIDTH, 35)
 BLUE_POSX = 50
 BLUE_POSY = HEIGHT-15
 RED_POSX = WIDTH-50
@@ -35,59 +37,81 @@ BOX_START_POS_RED = RED_POSX-BOX_LENGTH-1
 VEL_PENELTY = 1.1
 ARROW_LENGTH = 120
 ARROW_WIDTH = 20
+HP_WIDTH = 4
+
+HP_FONT = pygame.font.SysFont('comicsans', 12)
+WIND_FONT = pygame.font.SysFont('comicsans', 30)
 
 # https://www.freepik.com/vectors/alien-planet Alien planet vector created by upklyak
 BACKGROUND = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'BG.bmp')), (WIDTH, HEIGHT))
+    os.path.join('Assets', 'BG.jpg')), (WIDTH, HEIGHT))
 FLOOR = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'platform2.bmp')), (WIDTH, 25))
+    os.path.join('Assets', 'platform2.png')), (WIDTH, 25))
 
 '''The arrow only works between 1 to 179 degrees'''
 ARROW = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Arrow.bmp')), (ARROW_LENGTH, ARROW_WIDTH))
+    os.path.join('Assets', 'Arrow.png')), (ARROW_LENGTH, ARROW_WIDTH))
 
 # All animations are from Walfie https://walfiegif.wordpress.com
 GURA1 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Gura_1.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Gura_1.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 GURA2 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Gura_2.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Gura_2.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 GURA3 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Gura_3.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Gura_3.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 GURA4 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Gura_4.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Gura_4.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 GURA5 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Gura_5.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Gura_5.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 GURA6 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Gura_6.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Gura_6.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 GURA7 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Gura_7.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Gura_7.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 
 GURA = [GURA1, GURA2, GURA3, GURA4, GURA5, GURA6, GURA7]
 
 CALLIOPE1 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Calliope_1.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Calliope_1.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 CALLIOPE2 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Calliope_2.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Calliope_2.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 CALLIOPE3 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Calliope_3.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Calliope_3.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 CALLIOPE4 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Calliope_4.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Calliope_4.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 CALLIOPE5 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Calliope_5.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Calliope_5.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 CALLIOPE6 = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'Calliope_6.bmp')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    os.path.join('Assets', 'Calliope_6.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
 
 CALLIOPE = [CALLIOPE1, CALLIOPE2, CALLIOPE3, CALLIOPE4, CALLIOPE5, CALLIOPE6]
-print("Cover the light sensor and press the button...")
-while(True):
-    if(peri.get_switch()):
-        light_min = peri.get_light()
-        break
-print("Remove your hand and press the button...")
-while(True):
-    if(peri.get_switch()):
-        light_max = peri.get_light()
-        break
+
+GURA_SPIN1 = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'spin1.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+GURA_SPIN2 = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'spin2.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+GURA_SPIN3 = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'spin3.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+GURA_SPIN4 = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'spin4.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+GURA_SPIN5 = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'spin5.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+GURA_SPIN6 = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'spin6.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+GURA_SPIN7 = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'spin7.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+GURA_SPIN8 = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'spin8.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+GURA_SPIN9 = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'spin9.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+GURA_SPIN10 = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'spin10.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+GURA_SPIN11 = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'spin11.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+GURA_SPIN12 = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'spin12.png')), (PLAYER_WIDTH, PLAYER_HEIGHT))
+
+GURA_SPIN = [GURA_SPIN1, GURA_SPIN2, GURA_SPIN3,
+             GURA_SPIN4, GURA_SPIN5, GURA_SPIN6, GURA_SPIN7, GURA_SPIN8, GURA_SPIN9, GURA_SPIN10, GURA_SPIN11, GURA_SPIN12]
 
 
 class Box(object):
@@ -97,9 +121,11 @@ class Box(object):
         self.length = length
         self.color = color
 
+    def box(self):
+        return pygame.Rect(self.x, self.y, self.length, self.length)
+
     def draw(self, WIN):
-        box = pygame.Rect(self.x, self.y, self.length, self.length)
-        pygame.draw.rect(WIN, self.color, box)
+        pygame.draw.rect(WIN, self.color, self.box())
 
     def box_path(nowx, starty, velx, vely, time_relative, time_absolute, wind_speed):
 
@@ -112,15 +138,28 @@ class Box(object):
 
         return (newX, newY)
 
+    def touch_ground(self):
+        if self.y > HEIGHT - self.length - 1:
+            return True
 
-def draw_window(line, redBox, shoot, framenum, angle, player):
-    WIN.fill((255, 255, 255))
+    def collide_gura(self):
+        if (BLUE_POSX - PLAYER_WIDTH - PLAYER_WIDTH//2 < self.x < BLUE_POSX + PLAYER_WIDTH - PLAYER_WIDTH//2
+                and self.y + BOX_LENGTH > HEIGHT - PLAYER_HEIGHT):
+            return True
+
+    def collide_calli(self):
+        if (RED_POSX - PLAYER_WIDTH - PLAYER_WIDTH//2 < self.x < RED_POSX + PLAYER_WIDTH - PLAYER_WIDTH//2
+                and self.y + BOX_LENGTH > HEIGHT - PLAYER_HEIGHT):
+            return True
+
+
+def draw_window(redBox, shoot, framenum, angle, player, wind_spd, blue_hp, red_hp):
     WIN.blit(BACKGROUND, (0, 0))
     extra = 0
     if shoot:
         redBox.draw(WIN)
     if not shoot:
-        pygame.draw.line(WIN, BLACK, line[0], line[1])
+        # pygame.draw.line(WIN, BLACK, line[0], line[1])
         arrow = pygame.transform.rotate(ARROW, round((angle*180)/math.pi))
         if angle >= math.pi/2:
             extra = math.cos(angle)*ARROW_LENGTH
@@ -131,25 +170,55 @@ def draw_window(line, redBox, shoot, framenum, angle, player):
             WIN.blit(arrow, (BOX_START_POS_RED-(math.sin(angle)*10)+extra, HEIGHT -
                              BOX_LENGTH-(math.sin(angle)*ARROW_LENGTH)-40))
     pygame.draw.rect(WIN, BLACK, WALL)
-    WIN.blit(GURA[framenum//4 % len(GURA)],
-             (BLUE_POSX, BLUE_POSY-PLAYER_HEIGHT))
-    WIN.blit(CALLIOPE[framenum//4 % len(CALLIOPE)],
-             (RED_POSX-PLAYER_WIDTH, RED_POSY-PLAYER_HEIGHT))
+    if blue_hp > 0:
+        WIN.blit(GURA[framenum//4 % len(GURA)],
+                 (BLUE_POSX, BLUE_POSY-PLAYER_HEIGHT))
+    else:
+        WIN.blit(GURA_SPIN[framenum//4 % len(GURA_SPIN)],
+                 (BLUE_POSX, BLUE_POSY-PLAYER_HEIGHT))
+    if red_hp > 0:
+        WIN.blit(CALLIOPE[framenum//4 % len(CALLIOPE)],
+                 (RED_POSX-PLAYER_WIDTH, RED_POSY-PLAYER_HEIGHT))
+
     WIN.blit(FLOOR, (0, HEIGHT-15))
+
+    pygame.draw.rect(WIN, WHITE, WHITERECT)
+    wind_spd_text = HP_FONT.render("Wind Speed", 1, BLACK)
+    WIN.blit(wind_spd_text, (WIDTH//2-wind_spd_text.get_width()//2, 3))
+
+    if not shoot:
+        wind_spd_text2 = HP_FONT.render(str(wind_spd), 1, BLACK)
+    else:
+        wind_spd_text2 = HP_FONT.render("-", 1, BLACK)
+
+    WIN.blit(wind_spd_text2, (WIDTH//2-wind_spd_text2.get_width()//2, 18))
+    blue_hp_text = HP_FONT.render("HP: " + str(blue_hp), 1, BLACK)
+    WIN.blit(blue_hp_text, (3, 3))
+    red_hp_text = HP_FONT.render("HP: " + str(red_hp), 1, BLACK)
+    WIN.blit(red_hp_text, (WIDTH-red_hp_text.get_width()-3, 3))
+
+    for i in range(1, blue_hp):
+        blue_hpbar = pygame.Rect(3+(i*HP_WIDTH), 20, HP_WIDTH, 10)
+        pygame.draw.rect(WIN, (round(255-2.5*i), round(2.5*i), 0), blue_hpbar)
+
+    for i in range(1, red_hp):
+        blue_hpbar = pygame.Rect(WIDTH-(3+(i*HP_WIDTH)), 20, HP_WIDTH, 10)
+        pygame.draw.rect(WIN, (round(255-2.5*i), round(2.5*i), 0), blue_hpbar)
+
     pygame.display.update()
 
 
-def find_power(light):
+def find_power(light, light_min, light_max):
     if light > light_max:
-        power = 1000
+        power = 450
     elif light < light_min:
         power = 0
     else:
-        power = ((light-light_min)/(light_max-light_min))*1000
+        power = ((light-light_min)/(light_max-light_min))*600
     return power
 
 
-def find_angle(light):
+def find_angle(light, light_min, light_max):
     if light > light_max:
         angle = 179*math.pi/180
     elif light < light_min:
@@ -169,38 +238,41 @@ def block_collide_wall(po):
     return False
 
 
-def block_touch_ground(box):
-    if box.y > HEIGHT - box.length - 1:
-        return True
-
-
-# def handle_shooting(box, y, velx, vely,
-#                     BOX_FPS, time, wind_speed):
-
-#     shoot = True
-#     if box.y < HEIGHT - box.length:
-#         time += BOX_FPS
-#         if block_collide_wall(Box.box_path(box.x, y, velx, vely,
-#                                            BOX_FPS, time, wind_speed)):
-#             VEL_PENELTY = random.randint(20, 80)/100
-#             velx = -velx*VEL_PENELTY
-
-#             po = Box.box_path(box.x, y, velx, vely,
-#                               BOX_FPS, time, wind_speed)
-#             box.x = po[0]
-#             box.y = po[1]
-
-#     if box.y >= HEIGHT - box.length:
-#         shoot = False
-#         wind_speed = random.randint(-20, 20)
-#         print(wind_speed)
-#         box.y = HEIGHT-BOX_LENGTH - 51
-#         box.x = BOX_START_POS
-
-#     return (box.x, box.y, shoot, time)
+def change_player(color, redBox):
+    redBox.y = HEIGHT - BOX_LENGTH - 30
+    if color == 'RED':
+        redBox.x = BOX_START_POS_BLUE
+        peri.set_led(2, 0)
+        peri.set_led(0, 1)
+        return 'BLUE'
+    else:
+        redBox.x = BOX_START_POS_RED
+        peri.set_led(2, 1)
+        peri.set_led(0, 0)
+        return 'RED'
 
 
 def main():
+    print("Cover the light sensor and press the button...")
+    while(True):
+        if(peri.get_switch()):
+            light_min = peri.get_light()
+            break
+    while(peri.get_switch()):
+        continue
+    while(peri.get_switch()):
+        continue
+    print("lightmin=", light_min)
+    print("Remove your hand and press the button...")
+    while(True):
+        if(peri.get_switch()):
+            light_max = peri.get_light()
+            break
+    while(peri.get_switch()):
+        continue
+    while(peri.get_switch()):
+        continue
+    print("lightmax=", light_max)
     clock = pygame.time.Clock()
     redBox = Box(BOX_START_POS_BLUE, HEIGHT-BOX_LENGTH-30, BOX_LENGTH, WHITE)
     wind_speed = random.randint(-20, 20)
@@ -213,29 +285,38 @@ def main():
     shoot = False
     angset = False
     run = True
+    skill_used = False
     player = 'BLUE'
+    peri.set_led(2, 0)
     peri.set_led(0, 1)
+    blue_hp = 100
+    red_hp = 100
+    end_frame = -1
     while run:
 
         clock.tick(FPS)
-        pos = pygame.mouse.get_pos()
+        if (blue_hp <= 0 or red_hp <= 0) and end_frame == -1:
+            end_frame = framenum + 300
+        if end_frame != -1 and framenum >= end_frame:
+            run = False
+        pos = (450, HEIGHT-BOX_LENGTH-30)
         if player == 'BLUE':
             line = [(BOX_START_POS_BLUE, HEIGHT-BOX_LENGTH-30), pos]
         else:
             line = [(BOX_START_POS_RED, HEIGHT-BOX_LENGTH-30), pos]
 
         if shoot:
-            # nowPos = handle_shooting(redBox, y, velx, vely,
-            #                          BOX_FPS, time, wind_speed)
-            # redBox.x = nowPos[0]
-            # redBox.y = nowPos[1]
-            # shoot = nowPos[2]
-            # time = nowPos[3]
-
-            # print(redBox.x, redBox.y, shoot)
+            draw_window(redBox, shoot, framenum, angle,
+                        player, wind_speed, blue_hp, red_hp)
+            framenum = framenum+1
+            if(peri.get_switch()):
+                if not skill_used:
+                    print("skill used")
+                    velx = 0
+                    wind_speed = 0
+                    skill_used = True
             if redBox.y < HEIGHT - redBox.length:
                 time += BOX_FPS
-                print(velx)
                 if block_collide_wall(Box.box_path(redBox.x, y, velx, vely,
                                                    BOX_FPS, time, wind_speed)):
                     if (abs(velx) < 300):
@@ -247,47 +328,54 @@ def main():
                                   BOX_FPS, time, wind_speed)
                 redBox.x = po[0]
                 redBox.y = po[1]
-            elif block_touch_ground(redBox):
+            if redBox.touch_ground():
                 shoot = False
                 wind_speed = random.randint(-20, 20)
-                print('Wind speed =', wind_speed)
-                redBox.y = HEIGHT-BOX_LENGTH - 30
-                if player == 'RED':
-                    redBox.x = BOX_START_POS_BLUE
-                    peri.set_led(0, 1)
-                    peri.set_led(2, 0)
-                    player = 'BLUE'
-                else:
-                    redBox.x = BOX_START_POS_RED
-                    peri.set_led(0, 0)
-                    peri.set_led(2, 1)
-                    player = 'RED'
+                player = change_player(player, redBox)
+            elif (framenum > st_frame + 30):
+                if redBox.collide_gura():
+                    shoot = False
+                    blue_hp -= 30
+                    player = change_player(player, redBox)
+                elif redBox.collide_calli():
+                    shoot = False
+                    red_hp -= 30
+                    player = change_player(player, redBox)
         if not shoot:
             while(True):
                 light = peri.get_light()
                 if not angset:
-                    angle = find_angle(light)
+                    angle = find_angle(light, light_min, light_max)
+                    line[1] = (200*math.cos(angle), 200*math.sin(angle))
                 elif not shoot:
-                    power = find_power(light)/POWER_FACTOR
+                    power = find_power(
+                        light, light_min, light_max)/POWER_FACTOR
+                    line[1] = (power*math.cos(angle), power*math.sin(angle))
 
-                draw_window(line, redBox, shoot, framenum, angle, player)
+                draw_window(redBox, shoot, framenum, angle,
+                            player, wind_speed, blue_hp, red_hp)
                 framenum = framenum+1
 
                 if peri.get_switch():
+                    while(peri.get_switch()):
+                        continue
+                    while(peri.get_switch()):
+                        continue
                     if not angset:
                         angset = True
-                        angle = find_angle(light)
+                        angle = find_angle(light, light_min, light_max)
                     elif not shoot:
+                        skill_used = False
                         shoot = True
+                        angset = False
+                        st_frame = framenum
                         y = redBox.y
                         time = 0
-                        power = find_power()/POWER_FACTOR
+                        power = find_power(
+                            light, light_min, light_max)/POWER_FACTOR
                         velx = math.cos(angle) * power
                         vely = math.sin(angle) * power
                         break
-
-        draw_window(line, redBox, shoot, framenum, angle, player)
-        framenum = framenum+1
 
     pygame.quit()
 
